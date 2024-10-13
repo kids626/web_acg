a5ba2b8d7ef9e0c3eaf829af6be7a438f25094be
kids626 <kids626@gmail.com>
2024-10-14 01:22:53 +0800

    hot fix

--- app/Http/Controllers/RedisController.php
+++ app/Http/Controllers/RedisController.php
@@ -11,7 +11,7 @@ class RedisController extends Controller
     public function test_r(){
         //laravel_database_這個prefix其實是設定在上述的config/database.php中的REDIS_PREFIX
 
-        Redis::set('q1','我已將您訂購的商品放在指定位址');
+        Redis::set('q1','我已將您訂購的商品放在指定位置');
         echo Redis::get('q1');
         echo '<br>';
         Redis::set('q2','抱歉店家沒有給袋子,麻煩您要拿袋子出來裝');
